efbca899b525c4f69a6990743ceb172296f52dd4
jpleond11 <juanpablold1104@gmail.com>
2025-09-01 13:01:07 -0500

    Proyecto final (#15)
    * Feat: Estructura de codigo del caso veterinaria (#4)
    
    Se hace el merge del código con los cambios correctamente realizados.
    
    * Feat: Estructura de codigo del caso veterinaria (#5)
    
    Se realizan cambios en rama QA.
    
    * Feat clases estructura (#7)
    
    * Feat: Estructura de codigo del caso veterinaria
    
    * feat: se agregaron los cambios solicitados
    
    ---------
    
    Co-authored-by: jpleond11 <juanpablold1104@gmail.com>
    
    * Feat clases estructura (#8)
    
    * Feat: Estructura de codigo del caso veterinaria
    
    * Feat: Estructura de codigo del caso veterinaria (#4)
    
    Se hace el merge del código con los cambios correctamente realizados.
    
    * feat: se agregaron los cambios solicitados
    
    ---------
    
    Co-authored-by: jpleond11 <juanpablold1104@gmail.com>
    
    * Feat Clases Alternas
    
    * Feat clases alternas (#10)
    
    * Feat: Estructura de codigo del caso veterinaria (#5)
    
    Se realizan cambios en rama QA.
    
    * Feat: Estructura de codigo del caso veterinaria (#6)
    
    Se realiza merge final en la rama prod.
    
    * Feat clases estructura (#8)
    
    * Feat: Estructura de codigo del caso veterinaria
    
    * Feat: Estructura de codigo del caso veterinaria (#4)
    
    Se hace el merge del código con los cambios correctamente realizados.
    
    * feat: se agregaron los cambios solicitados
    
    ---------
    
    Co-authored-by: jpleond11 <juanpablold1104@gmail.com>
    
    * Feat clases estructura (#9)
    
    * Feat: Estructura de codigo del caso veterinaria
    
    * Feat: Estructura de codigo del caso veterinaria (#4)
    
    Se hace el merge del código con los cambios correctamente realizados.
    
    * Feat: Estructura de codigo del caso veterinaria (#5)
    
    Se realizan cambios en rama QA.
    
    * feat: se agregaron los cambios solicitados
    
    ---------
    
    Co-authored-by: jpleond11 <juanpablold1104@gmail.com>
    
    * Feat Clases Alternas
    
    ---------
    
    Co-authored-by: AndresVM1 <andresvilla6701@gmail.com>
    
    * Feat clases alternas (#11)
    
    * Feat: Estructura de codigo del caso veterinaria (#4)
    
    Se hace el merge del código con los cambios correctamente realizados.
    
    * Feat: Estructura de codigo del caso veterinaria (#6)
    
    Se realiza merge final en la rama prod.
    
    * Feat clases estructura (#7)
    
    * Feat: Estructura de codigo del caso veterinaria
    
    * feat: se agregaron los cambios solicitados
    
    ---------
    
    Co-authored-by: jpleond11 <juanpablold1104@gmail.com>
    
    * Feat clases estructura (#9)
    
    * Feat: Estructura de codigo del caso veterinaria
    
    * Feat: Estructura de codigo del caso veterinaria (#4)
    
    Se hace el merge del código con los cambios correctamente realizados.
    
    * Feat: Estructura de codigo del caso veterinaria (#5)
    
    Se realizan cambios en rama QA.
    
    * feat: se agregaron los cambios solicitados
    
    ---------
    
    Co-authored-by: jpleond11 <juanpablold1104@gmail.com>
    
    * Feat Clases Alternas
    
    ---------
    
    Co-authored-by: AndresVM1 <andresvilla6701@gmail.com>
    
    * Proyecto Final
    
    * Readme
    
    ---------
    
    Co-authored-by: AndresVM1 <andresvilla6701@gmail.com>

--- Clases/propietario.py
+++ Clases/propietario.py
@@ -1,9 +1,9 @@
 # Se crea la clase propietario del animal
 class Propietario:
-    def __init__(self, nombre, telefono, direccion) -> None:
+    def __init__(self, nombre: str, telefono: str, direccion: str) -> None:
         self.nombre = nombre
         self.telefono = telefono
         self.direccion = direccion
 
-    def mostrar_info(self):
-        return f"Nombre: {self.nombre}, TelÃ©fono: {self.telefono}, Dirección: {self.direccion}"
+    def mostrar_info(self) -> str:
+        return f"Nombre: {self.nombre}, Telefono: {self.telefono}, Dirección: {self.direccion}"
